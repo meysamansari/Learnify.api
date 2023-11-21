848f029b8bacece8a352fd8c36a6d302133747a1
meysamansari <meysamansari2023@gmail.com>
2023-11-21 18:34:35 +0330

    Add Auth routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::controller(AuthController::class)->group(function () {
+    Route::post('verification-code-request','sendVerificationCode');
+    Route::post('login/{type}','login');
+    Route::post('logout', 'logout');
+});
