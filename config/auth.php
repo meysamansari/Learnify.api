113910a3a61a7cc7d7466e0715d344b70926db90
meysamansari <meysamansari2023@gmail.com>
2023-11-21 09:46:30 +0330

    install sanctum and Configuration gaurd

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'web',
+        'guard' => 'sanctum',
         'passwords' => 'users',
     ],
 
@@ -40,6 +40,11 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'sanctum',
+            'provider' => 'users',
+            'hash' => false
+        ]
     ],
 
     /*
