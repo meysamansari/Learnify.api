c6c3c473a0f28366eaadb64e74971292eaac98a1
meysamansari <meysamansari2023@gmail.com>
2023-11-21 18:55:45 +0330

    Add sendVerificationCode for sending verify code with sms to login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\SmsVerificationCode;
+use App\Models\Verification;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-
+    public function sendVerificationCode(Request $request)
+    {
+        $phone_number = $request->input('phone_number');
+        $verification_code = rand(10000, 99999);
+        Verification::UpdateOrCreate(['phone_number' => $phone_number], ['verification_code' => $verification_code, 'verification_valid_until' => now()->addMinutes(5),]);
+        event(new SmsVerificationCode($phone_number, $verification_code));
+        return response()->json(['message' => 'Verification code send successfully']);
+    }
 }
