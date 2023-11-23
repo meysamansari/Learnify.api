f5adfeadf610eb337100731fa311e36dc0a676e7
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-11-23 16:57:13 +0330

    Add tables notes .

--- database/migrations/2023_11_23_082247_create_notes_table.php
+++ database/migrations/2023_11_23_082247_create_notes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('notes', function (Blueprint $table) {
             $table->id();
+            $table->integer('course_id');
+            $table->integer('user_id');
+            $table->string('description');
             $table->timestamps();
         });
     }
