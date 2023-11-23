a9144082d6b781cad49531a9c27ce231fc3672ca
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-11-23 16:58:32 +0330

    Add function store note .

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NoteController extends Controller
 {
-    //
+
+    public function store(Request $request)
+    {
+        $user = Auth::user();
+        return response()->json(Note::create([
+            'user_id' => $user->id,
+            'course_id' => $request->course_id,
+            'description' => $request->description
+
+        ]));
+    }
 }
