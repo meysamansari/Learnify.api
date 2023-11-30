b03b8cb00eefc583067f23dc4fa9a34ca9e48cd9
meysamansari <meysamansari2023@gmail.com>
2023-11-30 11:05:45 +0330

    add Update user function and use UserController in routes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
-    //
+    public function update(Request $request)
+    {
+        $user = Auth::user();
+
+        $phoneNumber = $user->phone_number;
+
+        $data = $request->except('_token', 'phone_number');
+        $update_user = User::where('phone_number', $phoneNumber)->get();
+        if ($update_user->isNotEmpty()) {
+            foreach ($update_user as $user) {
+                $user->update($data);
+            }
+            return response()->json(['message' => 'user information successfully updated', 'data' => $update_user]);
+        }
+        return response()->json(['message' => 'user not found'], 404);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NoteController;
 
