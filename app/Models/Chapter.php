8acb1ad12a88c8265596d5e538b4f9653fc0e5e3
meysamansari <meysamansari2023@gmail.com>
2023-11-30 11:23:39 +0330

    Add Chapter model and migration fillables with relations

--- app/Models/Chapter.php
+++ app/Models/Chapter.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Chapter extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'course_id',
+        'title',
+    ];
+    public function courses(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function lessons(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Lesson::class);
+    }
 }
--- database/migrations/2023_11_30_073839_create_chapters_table.php
+++ database/migrations/2023_11_30_073839_create_chapters_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('chapters', function (Blueprint $table) {
             $table->id();
+            $table->integer('course_id')->nullable();
+            $table->string('title')->nullable();
             $table->timestamps();
         });
     }
