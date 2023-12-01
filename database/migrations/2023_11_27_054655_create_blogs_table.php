1d75002ff3433a734fafaeaec8fe3b8be24a1652
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-12-01 23:57:56 +0330

    Create and compolet migration tables blog .

--- database/migrations/2023_11_27_054655_create_blogs_table.php
+++ database/migrations/2023_11_27_054655_create_blogs_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('blogs', function (Blueprint $table) {
             $table->id();
-
+            $table->string('title');
+            $table->longText('description');
             $table->timestamps();
         });
     }
