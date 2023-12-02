95d6202803983199e1b6eda4aeab4c61ec4f6ac9
meysamansari <meysamansari2023@gmail.com>
2023-12-02 10:17:11 +0330

    add course relation to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    public function courses(): \Illuminate\Database\Eloquent\Relations\HasMany
+    {
+        return $this->hasMany(Course::class);
+    }
 }
