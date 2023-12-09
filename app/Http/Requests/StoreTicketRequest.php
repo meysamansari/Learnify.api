9dd2ea85648e734803403761b9826a5c99234c2e
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-12-09 23:35:07 +0330

    Added fillables StoreTicketRequest .

--- app/Http/Requests/StoreTicketRequest.php
+++ app/Http/Requests/StoreTicketRequest.php
@@ -11,7 +11,7 @@ class StoreTicketRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,7 @@ class StoreTicketRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'message' => 'required'
         ];
     }
 }
