33a66f6e2e8808fda9dd8c3dfb86c1b866a61723
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-12-09 16:04:37 +0330

    Added fillables Comment to model and added relations with course and user .

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,8 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/**
+ * @method static create(array $array)
+ */
 class Comment extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'message',
+        'reply',
+        'course_id',
+        'user_id',
+        'rate'
+    ];
+
+    public function course(): BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+
+    }
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
