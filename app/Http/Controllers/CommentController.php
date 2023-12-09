39c49c020947598b1ffa215a9867d1cdde8420db
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-12-09 16:06:13 +0330

    Added function store comment .

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCommentRequest;
+use App\Models\Comment;
+use App\Models\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
-    //
+
+
+    public function store(StoreCommentRequest $request, $course_id)
+    {
+        $user = Auth::user();
+        Course::findOrFail($course_id);
+        $existingComment = $user->comments()->where('course_id', $course_id)->first();
+        $rate = $existingComment ? null : $request->rate;
+        $comment = Comment::create([
+            'message' => $request->message,
+            'reply' => null,
+            'course_id' => $course_id,
+            'user_id' => $user->id,
+            'rate' => $rate
+        ]);
+        return response()->json(['message' => 'Comment successfully created', 'data' => [$comment]]);
+    }
 }
