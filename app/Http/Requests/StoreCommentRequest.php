77a1cf695e7d65cc3837749aa4bf8d1d965189ae
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-12-09 16:08:45 +0330

    Added fillables StoreCommentRequest .

--- app/Http/Requests/StoreCommentRequest.php
+++ app/Http/Requests/StoreCommentRequest.php
@@ -11,7 +11,7 @@ class StoreCommentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class StoreCommentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'message' => 'required',
+            'rate' => 'required|in:1,2,3,4,5'
         ];
     }
 }
