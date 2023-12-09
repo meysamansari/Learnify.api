d857c3aa0161a7ea4d54d009c451fec12b184b5a
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-12-09 23:31:50 +0330

    Added fillables model ticket .

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -5,7 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @method static create(array $array)
+ */
 class Ticket extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'message',
+        'reply',
+        'course_id',
+        'user_id',
+    ];
+
+
+
+    public function course(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(Course::class);
+    }
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
