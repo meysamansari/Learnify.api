eff3a272049546692da1255fc80be5b7823feef1
muhammadreza <145956819+murzaaaaaaaaaa@users.noreply.github.com>
2023-12-09 23:34:21 +0330

    Added function store ticket .

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTicketRequest;
+use App\Models\Course;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TicketController extends Controller
 {
-    //
+
+
+
+    public function store(StoreTicketRequest $request, $course_id)
+    {
+        $user = Auth::user();
+        Course::findOrFail($course_id);
+        $ticket = Ticket::create([
+            'message' => $request->message,
+            'reply' => null,
+            'course_id' => $course_id,
+            'user_id' => $user->id,
+        ]);
+        return response()->json(['message' => 'Ticket successfully created', 'data' => [$ticket]]);
+    }
 }
