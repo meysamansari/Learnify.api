e946873cc7d6ef12c9eb458f99299776a0c2e08b
meysamansari <meysamansari2023@gmail.com>
2023-12-10 16:18:54 +0330

    create VerificationRequest

---
+++ app/Http/Requests/VerificationRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class VerificationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'phone_number' => 'required|regex:/^09[0-9]{9}$/',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'phone_number.regex' => "phone_number isn't correct",
+        ];
+    }
+}
