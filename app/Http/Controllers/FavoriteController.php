3ae163599d55857d953751840d16b764a97f7ad0
meysamansari <meysamansari2023@gmail.com>
2023-12-10 18:20:40 +0330

    add show Favorite function

--- app/Http/Controllers/FavoriteController.php
+++ app/Http/Controllers/FavoriteController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Favorite;
 use Illuminate\Http\Request;
 
 class FavoriteController extends Controller
 {
-    //
+    public function index(){
+        $favorites = Favorite::get();
+        return response()->json(['favorites' => $favorites]);
+    }
 }
