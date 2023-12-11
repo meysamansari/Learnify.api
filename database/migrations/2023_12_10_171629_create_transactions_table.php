bd15f92289cabbb9cffe7fbe1e93ff8e2e4fec66
meysamansari <meysamansari2023@gmail.com>
2023-12-11 10:50:46 +0330

    create Transactions migration

---
+++ database/migrations/2023_12_10_171629_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('order_id');
+            $table->json('gateway_result');
+            $table->unsignedBigInteger('total_price')->nullable();
+            $table->enum('status', ['failed', 'pending', 'paid']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
