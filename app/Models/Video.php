11831a04c89326bb9429b6ccac7c2028622d1ef4
meysamansari <meysamansari2023@gmail.com>
2023-12-13 10:21:47 +0330

    add relationship

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
@@ -12,9 +13,15 @@ class Video extends Model implements HasMedia
     use HasFactory,InteractsWithMedia;
     protected $fillable = [
         'status',
+        'videoable_id',
+        'videoable_type'
         ];
     public function registerMediaCollections(): void
     {
         $this->addMediaCollection('video');
+    }
+    public function videoable(): MorphTo
+    {
+        return $this->morphTo();
     }
 }
